fd9dcd2b7048b5691425e621df6c2972eea5d6a8
emanny0018 <95097412+emanny0018@users.noreply.github.com>
2024-08-20 14:30:36 -0400

    Update predict_match.py

--- scripts/predict_match.py
+++ scripts/predict_match.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from joblib import load
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score
 
 def load_data(file_path):
     """Load dataset from a file."""
@@ -34,7 +34,7 @@ def compare_team_matches(df, home_team, away_team):
         away_team_name = match['Away'].capitalize()
         home_goals = match['HomeGoals']
         away_goals = match['AwayGoals']
-        actual_result = 'Home Win' if match['FTR'] == 'H' else ('Away Win' if match['FTR'] == 'A' else 'Draw')
+        actual_result = 'Home Win' if match.get('Target') == 0 else ('Away Win' if match.get('Target') == 1 else 'Draw')
 
         print(f"{match_date}: {home_team_name} {home_goals}-{away_goals} {away_team_name} (Actual: {actual_result})")
 
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     print(f"Predicted Score Probability: Home Win {predicted_proba[0][0]:.2f}, Away Win {predicted_proba[0][1]:.2f}, Draw {predicted_proba[0][2]:.2f}")
     
     # Actual result from the dataset (to calculate accuracy)
-    actual_result = 'Home Win' if features['FTR'] == 'H' else ('Away Win' if features['FTR'] == 'A' else 'Draw')
+    actual_result = 'Home Win' if features['Target'] == 0 else ('Away Win' if features['Target'] == 1 else 'Draw')
     
     # Calculate accuracy based on actual result
     accuracy = accuracy_score([actual_result], [predicted_result])
